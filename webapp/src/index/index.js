7b7e20d48eee26f9b31640f013709c788d5633bf
zhouyou <zhouyou.world@outlook.com>
2017-11-26 20:21:51 +0800

    前端webpack配置文件

---
+++ webapp/src/index/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: zhouyou@weruan 
+ * @Descriptions: 首页js依赖文件
+ * @Date: 2017-11-26 20:01:16 
+ * @Last Modified by: zhouyou@weruan
+ * @Last Modified time: 2017-11-26 20:03:24
+ */
+
+//import scss
+require("./index.scss");
