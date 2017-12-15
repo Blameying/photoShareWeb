78fb3ea53c8196527ceeadcf853de6ec9fd39fcb
zhouyou <zhouyou.world@outlook.com>
2017-12-15 16:22:11 +0800

    首页前端页面实现

--- webapp/src/index/index.js
+++ webapp/src/index/index.js
@@ -3,8 +3,31 @@
  * @Descriptions: 首页js依赖文件
  * @Date: 2017-11-26 20:01:16 
  * @Last Modified by: zhouyou@weruan
- * @Last Modified time: 2017-11-26 20:03:24
+ * @Last Modified time: 2017-12-15 00:59:17
  */
 
 //import scss
 require("./index.scss");
+
+
+//import js
+require("../js/jquery.backstretch.js");
+
+
+(function () {
+    // img 依赖
+    const jpg1 = require("./img/banner1.jpg");
+    const jpg2 = require("./img/banner2.jpg");
+    const jpg3 = require("./img/banner3.jpg");
+    const jpg4 = require("./img/banner4.jpg");
+
+    // Handle Backstretch 背景轮换
+    jQuery.backstretch([jpg1, jpg2, jpg3, jpg4], {
+        duration: 5000,
+        fade: 750,
+        scale: "fade",
+        alwaysTestWindowResolution: true
+    });
+
+
+})();
