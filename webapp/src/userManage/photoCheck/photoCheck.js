25671e28666a04bb4531becc1dd9f41214510dbe
zhouyou <zhouyou.world@outlook.com>
2017-12-18 10:53:27 +0800

    完成图片审核界面

--- webapp/src/userManage/photoCheck/photoCheck.js
+++ webapp/src/userManage/photoCheck/photoCheck.js
@@ -3,7 +3,7 @@
  * @Descriptions: 图片审核界面js依赖文件
  * @Date: 2017-12-18 00:44:09 
  * @Last Modified by: zhouyou@weruan
- * @Last Modified time: 2017-12-18 00:47:19
+ * @Last Modified time: 2017-12-18 09:36:35
  */
 
 //get the view
@@ -11,6 +11,9 @@ const photoCheck = require("./photoCheck.html");
 
 //import css
 require("./photoCheck.scss");
+
+//import js
+require("../../js/iconfont.js");
 
 // 路由调用
 SPA_RESOLVE_INIT = function(transition) {
